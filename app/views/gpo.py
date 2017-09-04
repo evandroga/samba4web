65e8491ab6cac01b2080fcc4dbe14f087c30beff
Evandro Gomes <evandroga@hotmail.com>
2017-09-04 17:16:40 -0300

    adicionado funcionalidade de listagem das GPO's

--- app/views/gpo.py
+++ app/views/gpo.py
@@ -1,10 +1,15 @@
 from flask import Blueprint, render_template
+from .. import execProcess
 
 gpo = Blueprint('gpo', __name__)
 
 @gpo.route('/')
 def showGPO():
-    render_template('gpo.html')
+    out, c = execProcess(('samba-tool',
+                          'gpo',
+                          'listall'))
+    if c == 0:
+        render_template('gpo.html', gpolist=out)
 
 @gpo.route('/add')
 def addGPO():
