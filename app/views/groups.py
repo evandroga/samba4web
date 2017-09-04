b59a9dff6b03059fdc5042b710ac1a7ac68d089e
Evandro Gomes <evandroga@hotmail.com>
2017-09-04 17:10:15 -0300

    adicionado processo de listagem de grupos

--- app/views/groups.py
+++ app/views/groups.py
@@ -1,10 +1,15 @@
 from flask import Blueprint, render_template
+from .. import execProcess
 
 groups = Blueprint('groups', __name__)
 
 @groups.route('/')
 def showGroups():
-    render_template('groups.html')
+    out, c = execProcess(('samba-tool',
+                          'group',
+                          'list'))
+    if c == 0:
+        render_template('groups.html', groupslist=out)
 
 @groups.route('/add')
 def addGroups():
