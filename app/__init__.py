fb3749f60b86aee68487a3d112cbe28195b1071d
Evandro Gomes <evandroga@hotmail.com>
2017-09-04 17:25:46 -0300

    Adicionado prefixos nos blueprints gerados

--- app/__init__.py
+++ app/__init__.py
@@ -6,10 +6,10 @@ from .views.groups import groups
 from .views.gpo import gpo
 
 app = Flask(__name__)
-app.register_blueprint(login)
-app.register_blueprint(users)
-app.register_blueprint(groups)
-app.register_blueprint(gpo)
+app.register_blueprint(login, url_prefix='/login')
+app.register_blueprint(users, url_prefix='/users')
+app.register_blueprint(groups, url_prefix='/groups')
+app.register_blueprint(gpo, url_prefix='/gpo')
 
 
 def execProcess(command):
